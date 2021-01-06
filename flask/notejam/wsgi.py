9f8ed49418c3108eb6e37c6efee13d9706b712b1
Greg Nagy <ngergo@users.noreply.github.com>
2021-01-06 12:16:53 +0100

    Revert "notejam-demo"

--- flask/notejam/wsgi.py
+++
@@ -1,7 +0,0 @@
-from notejam import app as application
-from notejam.config import DevelopmentConfig
-
-application.config.from_object(DevelopmentConfig)
-
-if __name__ == '__main__':
-    application.run()
